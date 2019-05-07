d0ff01c25109eababf1293258f1f2f6a319fc1f4
Jesus Mejia <jesusmejded@gmail.com>
2019-05-07 18:31:52 -0500

    testing PMM data

--- main.jl
+++ main.jl
@@ -1,23 +1,60 @@
 using Bilevel, BilevelBenchmark
+import Printf.@printf
+using Suppressor
+import LinearAlgebra.norm
+using JLD
 
-function getBilevel(fnum, D)
-    F(x, y) = PMM_leader(x, y, fnum)
-    f(x, y) = PMM_follower(x, y, fnum)
+NRUNS = 1
 
-    bounds = Array([ -10ones(D) 10ones(D) ]')
-
-    return F, f, bounds, bounds
+function configure(output_dir="./")
+    all_data_dir = string(output_dir, "output")
+    summary_data_dir = string(output_dir, "summary")
+    
+    !isdir(all_data_dir)     && mkdir(all_data_dir)
+    !isdir(summary_data_dir) && mkdir(summary_data_dir)
 
 end
 
+function getBilevel(fnum, D_ul, D_ll)
+    F(x, y) = PMM_leader(x, y, fnum)
+    f(x, y) = PMM_follower(x, y, fnum)
+
+    bounds_ul = Array([ -10ones(D_ul) 10ones(D_ul) ]')
+    bounds_ll = Array([ -10ones(D_ll) 10ones(D_ll) ]')
+
+    return F, f, bounds_ul, bounds_ll
+
+end
+
+function getQBCA(fnum, D_ul, D_ll)
+    F, f, bounds_ul, bounds_ll = getBilevel(fnum, D_ul, D_ll)
+    method = QBCA(size(bounds_ul, 2); options = Bilevel.Options(F_tol=1e-4, f_tol=1e-4, store_convergence=true))
+    result = nothing
+    @suppress begin
+        result = optimize(F, f, bounds_ul, bounds_ll, method, Bilevel.Information(F_optimum=0.0, f_optimum=0.0))
+    end
+    result
+end
+
 function main()
-    fnum = 1
-    D = 6
+    D_ul = 2
+    D_ll = 3
+    configure()
 
-    F, f, bounds_ul, bounds_ll = getBilevel(fnum, D)
-
-    optimize(F, f, bounds_ul, bounds_ll, QBCA(D))
-
+    for fnum = 1:5
+        F, f, bounds_ul, bounds_ll = getBilevel(fnum, D_ul, D_ll)
+        result_list = []
+        for r = 1:NRUNS
+            result = getQBCA(fnum, D_ul, D_ll)
+            @printf("PMM%d \t run = %d \t F = %.4e \t f = %.4e \t ‖x‖ = %.4e  ‖y‖ = %.4e \n", fnum, r, result.best_sol.F, result.best_sol.f, norm(result.best_sol.x), norm(result.best_sol.y))
+            println("x = ", result.best_sol.x)
+            println("y = ", result.best_sol.y)
+            # store data
+            # save("output/SMD_$(D_ul)_$(D_ll)/SMD$(fnum)_r$(r).jld", "result", result)
+            push!(result_list, result)
+        end
+        # save("output/SMD_$(D_ul)_$(D_ll)/SMD$(fnum).jld", "result_list", result_list)
+    end
 end
 
 main()
